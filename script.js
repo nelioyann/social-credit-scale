705e5f78c0085c092c9d7050c147fb10d34603e4
nelioyann <nanayannick1998@gmail.com>
2020-02-29 16:59:58 +0100

    hehehe

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("object");
     const boutonEnvoyer = document.querySelector("#calculate");
     const formulaire = document.querySelector("form");
+    const bar = document.querySelector("#bar");
     const compter = e => {
-        // e.preventDefault();
+        e.preventDefault();
         // console.log(e)
         var inputs = document.querySelectorAll('input[type="radio"]');
         var total = 0;
@@ -15,8 +16,10 @@ window.addEventListener("DOMContentLoaded", () => {
                 total += 1;
             }
         }
+        bar.style.width = (total*10)+1 + "%"
         let score = total * 45 + 350;
-        alert("Ton score est de" + "\n" + score);
+        bar.innerHTML = score
+        // alert("Ton score est de" + "\n" + score);
         // formulaire.unbind('submit').submit()
     };
     // if score inf
